c203cfe723eed7c5125787b2850cfa1c0ecd6dbd
gajewskp <paula.gajewska-behnke@vimn.com>
2020-09-09 08:27:31 +0200

    remove Person

--- critter/src/main/java/com/udacity/jdnd/course3/critter/user/entity/Customer.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/user/entity/Customer.java
@@ -3,9 +3,13 @@ package com.udacity.jdnd.course3.critter.user.entity;
 import com.udacity.jdnd.course3.critter.pet.entity.Pet;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +17,14 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @Entity
-public class Customer extends Person {
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Nationalized
+    private String name;
 
     private String phoneNumber;
     private String notes;
--- critter/src/main/java/com/udacity/jdnd/course3/critter/user/entity/Employee.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/user/entity/Employee.java
@@ -3,9 +3,13 @@ package com.udacity.jdnd.course3.critter.user.entity;
 import com.udacity.jdnd.course3.critter.user.EmployeeSkill;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Nationalized;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.DayOfWeek;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,7 +17,14 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @Entity
-public class Employee extends Person {
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Nationalized
+    private String name;
 
     @ElementCollection
     private Set<EmployeeSkill> skills = new HashSet<>();
